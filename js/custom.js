5ce8cf324561d2c0cee3aebddff5db996dfd472b
Wojak94 <wojciechadles@gmail.com>
2016-07-20 12:07:11 +0200

    dodany tekst on hover

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,20 @@
+//
+// $('#sidebar').affix({
+//     offset: {
+//         top: $('#top_navbar').offset().top(),
+//         left: 0
+//     }
+// });
 
-$('#sidebar').affix({
-    offset: {
-        top: $('#top_navbar').offset().top(),
-        left: 0
-    }
+$('.thumbnail').hover(
+
+    function () {
+        $(this).find('.caption').stop().fadeOut();
+        $(this).find('.caption-hover').stop().fadeIn();
+
+    },
+
+    function () {
+        $(this).find('.caption').stop().fadeIn();
+        $(this).find('.caption-hover').stop().fadeOut();
 });
-
-
