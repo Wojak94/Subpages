0ea6204738901a84813c39df84af199abc07e914
Wojak94 <wojciechadles@gmail.com>
2016-07-23 04:02:48 +0200

    TODO: CoverFlow wyjątki (kliknięty pierwszy albo ostatni element)

--- js/custom.js
+++ js/custom.js
@@ -19,7 +19,7 @@ var json = {"images": [
         "title": "Title 2",
         "price": "3600",
         "captionHover": "asdasdasd",
-        "caption": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "
+        "caption": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "
     },
     {
         "src": "./img/Firefox_wallpaper.png",
@@ -33,14 +33,14 @@ var json = {"images": [
         "title": "Title 4",
         "price": "800",
         "captionHover": "asdasdasd",
-        "caption": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "
+        "caption": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "
     },
     {
         "src": "./img/Firefox_wallpaper.png",
         "title": "Title 5",
         "price": "9999",
         "captionHover": "asdasdasd",
-        "caption": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "
+        "caption": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
     },
     {
         "src": "./img/death-breath.jpg",
@@ -61,12 +61,16 @@ var json = {"images": [
         "title": "Title 8",
         "price": "3400",
         "captionHover": "title hov",
-        "caption": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "
+        "caption": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "
     }
 ]};
 
 
 var mainImgId = 0;
+
+$(document).ready(function () {
+    $('#metro').fadeIn(1000).removeClass('hidden');
+});
 
 window.onload = function drawAuctions(){
 
@@ -119,7 +123,6 @@ $('#metro').on('click', '.thumbnail', function () {
 
 });
 
-
 $('#main-content').on('click','#nextImgCol .img-responsive', function () {
         var imgSrcPrev = json.images[mainImgId].src;
         var imgSrc = json.images[mainImgId+1].src;
@@ -128,6 +131,10 @@ $('#main-content').on('click','#nextImgCol .img-responsive', function () {
         var h3 = json.images[mainImgId+1].title;
         var p = json.images[mainImgId+1].caption;
         var price = json.images[mainImgId+1].price;
+
+        $('#previousImgWrap .img-responsive').removeClass('hidden');
+        $('#previousImgWrap .arrowPrev').removeClass('hidden');
+
 
         $('#previousImgWrap').fadeOut(function () {
         $('#previousImgWrap .img-responsive').attr('src', imgSrcPrev);});
@@ -144,11 +151,15 @@ $('#main-content').on('click','#nextImgCol .img-responsive', function () {
             if(mainImgId+2 < json.images.length){
             $('#nextImgCol .img-responsive').attr('src', imgSrcNext);}
         });
+
+        $('#bidSection').fadeOut();
+
         if(mainImgId == 0){$('#previousImgWrap ').delay(400).fadeIn(500);}
         else{$('#previousImgWrap ').fadeIn(500);}
         $('#mainImgCol').fadeIn();
         $('#mainCapCol').fadeIn();
         if(mainImgId+2 < json.images.length){$('#nextImgWrap').fadeIn(500);}
+         $('#bidSection').fadeIn(500);
 
         mainImgId++;
 });
@@ -162,6 +173,9 @@ $('#main-content').on('click','#previousImgCol .img-responsive', function () {
         var h3 = json.images[mainImgId-1].title;
         var p = json.images[mainImgId-1].caption;
         var price = json.images[mainImgId-1].price;
+
+        $('#nextImgWrap .img-responsive').removeClass('hidden');
+        $('#nextImgWrap .arrowNext').removeClass('hidden');
 
         $('#nextImgWrap').fadeOut(function () {
             $('#nextImgWrap .img-responsive').attr('src', imgSrcNext);});
@@ -178,27 +192,53 @@ $('#main-content').on('click','#previousImgCol .img-responsive', function () {
             if(mainImgId-2 >= 0){
                 $('#previousImgWrap .img-responsive').attr('src', imgSrcPrev);}
         });
+
+        $('#bidSection').fadeOut();
+
         if(mainImgId == json.images.length-1){$('#nextImgWrap ').delay(400).fadeIn(500);}
         else{$('#nextImgWrap').fadeIn(500);}
         $('#mainImgCol').fadeIn();
         $('#mainCapCol').fadeIn();
         if(mainImgId-2 >= 0){$('#previousImgWrap').fadeIn(500);}
+        $('#bidSection').fadeIn(500);
+
 
         mainImgId--;
 });
 
 
     function loadCoverFlow(mainThumId) {
-    if(mainThumId-1 >= 0) {
+
         var prevImg = document.createElement('img');
-        prevImg.className = 'img-responsive';
+        var arrowPrev = document.createElement('div');
+
+
+
+    if(mainThumId-1 >= 0){
         prevImg.src = json.images[mainThumId - 1].src;
+        prevImg.className = 'img-responsive';
+        arrowPrev.className = 'arrowPrev';
+    }
+    else {
+        prevImg.src = '';
+        prevImg.className = 'img-responsive hidden';
+        arrowPrev.className = 'arrowPrev hidden';
     }
 
-    if(mainThumId+1 < json.images.length){
         var nextImg = document.createElement('img');
-        nextImg.className = 'img-responsive';
+        var arrowNext = document.createElement('div');
+
+
+    if(mainThumId+1 < json.images.length){
         nextImg.src = json.images[mainThumId + 1].src;
+        nextImg.className = 'img-responsive';
+        arrowNext.className = 'arrowNext';
+
+    }
+    else{
+        nextImg.src = '';
+        nextImg.className = 'img-responsive hidden';
+        arrowNext.className = 'arrowNext hidden';
     }
 
     var prevImgCol = document.createElement('div');
@@ -216,7 +256,6 @@ $('#main-content').on('click','#previousImgCol .img-responsive', function () {
     nextImgCol.className = 'col-xs-3';
     nextImgCol.id = 'nextImgCol';
 
-
     var nextImgWrap = document.createElement('div');
     nextImgWrap.id = 'nextImgWrap';
 
@@ -225,22 +264,16 @@ $('#main-content').on('click','#previousImgCol .img-responsive', function () {
     mainImg.src = json.images[mainThumId].src;
 
     var prevCapCol = document.createElement('div');
-    prevCapCol.className = 'col-xs-3';
+    prevCapCol.className = 'col-xs-2 col-md-3';
     prevCapCol.id = 'previousCapCol';
 
-    var arrowPrev = document.createElement('div');
-    arrowPrev.className = 'arrowPrev';
-
     var mainCapCol = document.createElement('div');
-    mainCapCol.className = 'col-xs-6';
+    mainCapCol.className = 'col-xs-8 col-md-6';
     mainCapCol.id = 'mainCapCol';
 
     var nextCapCol = document.createElement('div');
-    nextCapCol.className = 'col-xs-3';
+    nextCapCol.className = 'col-xs-2 col-md-3';
     nextCapCol.id = 'nextCapCol';
-
-    var arrowNext = document.createElement('div');
-    arrowNext.className = 'arrowNext';
 
     var capCovFlow = document.createElement('div');
     capCovFlow.className = 'title covFlow';
@@ -254,21 +287,55 @@ $('#main-content').on('click','#previousImgCol .img-responsive', function () {
     var capCovPrice = document.createElement('h1');
     capCovPrice.innerHTML = json.images[mainThumId].price + " zł";
 
+    var bidSection = document.createElement('div');
+    bidSection.className = 'col-xs-12';
+    bidSection.id = 'bidSection';
+
+    var leftBid = document.createElement('div');
+    leftBid.className = 'col-xs-3';
+    leftBid.id = 'leftBid';
+
+    var mainBid = document.createElement('div');
+    mainBid.className = 'col-xs-6';
+    mainBid.id = 'mainBid';
+
+    var rightBid = document.createElement('div');
+    rightBid.className = 'col-xs-3';
+    rightBid.id = 'rightBid';
+
+    var inputGroup = document.createElement('div');
+    inputGroup.className = 'input-group';
+    inputGroup.id = 'bid-input';
+
+    var input = document.createElement('input');
+    input.type = 'number';
+    input.className = 'form-control';
+
+    var spanInput = document.createElement('span');
+    spanInput.className = 'input-group-addon';
+    spanInput.innerHTML = "PLN";
+
+    var buttonBid = document.createElement('button');
+    buttonBid.type = 'button';
+    buttonBid.className = 'btn btn-success';
+    buttonBid.id = 'bid-button';
+
+    var h4BtnBid = document.createElement('h4');
+    h4BtnBid.innerHTML = "LICYTUJ";
+
+
+
     var mainRow = document.getElementById("main-row");
     mainRow.appendChild(prevImgCol);
-    if(mainThumId-1 >= 0) {
         prevImgCol.appendChild(prevImgWrap);
-        prevImgWrap.appendChild(prevImg)
+        prevImgWrap.appendChild(prevImg);
         prevImgWrap.appendChild(arrowPrev);
-    }
     mainRow.appendChild(mainImgCol);
     mainImgCol.appendChild(mainImg);
     mainRow.appendChild(nextImgCol);
-    if(mainThumId+1 < json.images.length) {
         nextImgCol.appendChild(nextImgWrap);
-        nextImgWrap.appendChild(nextImg)
+        nextImgWrap.appendChild(nextImg);
         nextImgWrap.appendChild(arrowNext);
-    }
     mainRow.appendChild(prevCapCol);
     mainRow.appendChild(mainCapCol);
     mainCapCol.appendChild(capCovFlow);
@@ -276,6 +343,14 @@ $('#main-content').on('click','#previousImgCol .img-responsive', function () {
     capCovFlow.appendChild(capCov);
     capCovFlow.appendChild(capCovPrice);
     mainRow.appendChild(nextCapCol);
+    mainRow.appendChild(bidSection);
+    bidSection.appendChild(leftBid);
+    bidSection.appendChild(mainBid);
+    mainBid.appendChild(inputGroup);
+    inputGroup.appendChild(input);
+    inputGroup.appendChild(spanInput);
+    mainBid.appendChild(buttonBid);
+    buttonBid.appendChild(h4BtnBid);
 }
 
 }());
