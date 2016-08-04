ba88a7ea5c3b28435e76ab24932f1c2108496c78
Wojak94 <wojciechadles@gmail.com>
2016-08-04 14:06:56 +0200

    Przywracanie z historii działa.

--- js/custom.js
+++ js/custom.js
@@ -82,7 +82,7 @@ window.addEventListener("popstate", function(event) {
 
         if ((stateObject.url).indexOf('covFlow') != -1){                            //else check url stored in history.state if it contains 'covFlow' substring
             var imgId = (stateObject.url).charAt(stateObject.url.length - 1);       //if so, gather last char which is auction number
-            loadCoverFlow(imgId);                                                   //and pass it to Coverflow drawing function
+            loadCoverFlow(imgId);                                                   //and pass it to coverFlow drawing function
         }
     }
 });
@@ -98,7 +98,7 @@ $("#logoBtn").click(function(e) {
 function drawAuctions(){
 
 if($('#main-row').length){                                                          //if current page contains #main-row div
-    $('#main-row').fadeOut(300, function(){                                         //which means its Coverflow page, remove it
+    $('#main-row').fadeOut(300, function(){                                         //which means its coverFlow page, remove it
             $('#main-row').remove();
 })}
 
@@ -107,14 +107,14 @@ if($('#mainContentWrap').length){                                               
         return;
 }
        var mainContentWrap = document.createElement('div');
-        mainContentWrap.className = 'hidden';                                       //setting
+        mainContentWrap.className = 'hidden';                                       //setting div to hidden for fadeIn when its ready
         mainContentWrap.id = 'mainContentWrap';
 
         var container = document.getElementById("main-content");
         container.appendChild(mainContentWrap);
 
-    for(var i=0; i<json.images.length; i++) {
-
+    for(var i=0; i<json.images.length; i++) {                                       //main loop that draws each thumbnail until end of json
+                                                                                    //storing each auction
         var thumbCol = document.createElement('div');
         thumbCol.className = 'col-xs-12 col-sm-4 col-md-3 thumbCol';
 
@@ -154,269 +154,29 @@ if($('#mainContentWrap').length){                                               
         capHov.appendChild(p2);
         thumb.appendChild(shadow);
     }
-    $(document).ready(function () {
+    $(document).ready(function () {                                                 //when populating page is done, fadeIn content
         $('#mainContentWrap').fadeIn(1000).removeClass('hidden');
     });
 }
 
 //-------------------------------------------------------------------Click on auction thumbnail event handler
-$('#main-content').on('click', '.thumbnail', function () {
-    mainId = parseInt(jQuery(this).attr('id'));
 
+$('#main-content').on('click', '.thumbnail', function () {
+    mainId = parseInt(jQuery(this).attr('id'));                                     //sets global var containing id of main img in coverFlow
+                                                                                    //from id of clicked thumbnail
     $('#mainContentWrap').fadeOut( function(){
-        loadCoverFlow(mainId);
+        loadCoverFlow(mainId);                                                      //fadeOut of main content, then draw coverFlow
     });
-    history.pushState({url: '#covFlow' + mainId}, null, '#covFlow' + mainId);
+    history.pushState({url: '#covFlow' + mainId}, null, '#covFlow' + mainId);       //pushing history state of loaded covFlow main image
 });
 
-
-//-------------------------------------------------------------------Coveflow drawing function
-//     function loadCoverFlow(Id) {
-//         var mainThumId = parseInt(Id);
-//
-//         var prevImg = document.createElement('img');
-//         var arrowPrev = document.createElement('div');
-//
-//         if(mainThumId-1 >= 0){
-//             prevImg.src = json.images[mainThumId - 1].src;
-//             prevImg.className = 'img-responsive';
-//             arrowPrev.className = 'arrowPrev';
-//         }
-//         else {
-//             prevImg.src = '';
-//             prevImg.className = 'img-responsive hidden';
-//             arrowPrev.className = 'arrowPrev hidden';
-//         }
-//
-//         var nextImg = document.createElement('img');
-//         var arrowNext = document.createElement('div');
-//
-//
-//         if(mainThumId+1 < json.images.length){
-//             nextImg.src = json.images[mainThumId + 1].src;
-//             nextImg.className = 'img-responsive';
-//             arrowNext.className = 'arrowNext';
-//         }
-//         else{
-//             nextImg.src = '';
-//             nextImg.className = 'img-responsive hidden';
-//             arrowNext.className = 'arrowNext hidden';
-//         }
-//
-//         var prevImgCol = document.createElement('div');
-//         prevImgCol.className = 'col-xs-3';
-//         prevImgCol.id = 'previousImgCol';
-//
-//         var prevImgWrap = document.createElement('div');
-//         prevImgWrap.id = 'previousImgWrap';
-//
-//         var mainImgCol = document.createElement('div');
-//         mainImgCol.className = 'col-xs-6';
-//         mainImgCol.id = 'mainImgCol';
-//
-//         var nextImgCol = document.createElement('div');
-//         nextImgCol.className = 'col-xs-3';
-//         nextImgCol.id = 'nextImgCol';
-//
-//         var nextImgWrap = document.createElement('div');
-//         nextImgWrap.id = 'nextImgWrap';
-//
-//         var mainImg = document.createElement('img');
-//         mainImg.className = 'img-responsive';
-//         mainImg.src = json.images[mainThumId].src;
-//
-//         var prevCapCol = document.createElement('div');
-//         prevCapCol.className = 'col-xs-1 col-md-3';
-//         prevCapCol.id = 'previousCapCol';
-//
-//         var mainCapCol = document.createElement('div');
-//         mainCapCol.className = 'col-xs-10 col-md-6';
-//         mainCapCol.id = 'mainCapCol';
-//
-//         var nextCapCol = document.createElement('div');
-//         nextCapCol.className = 'col-xs-1 col-md-3';
-//         nextCapCol.id = 'nextCapCol';
-//
-//         var capCovFlow = document.createElement('div');
-//         capCovFlow.className = 'title covFlow';
-//
-//         var capCovHead = document.createElement('h3');
-//         capCovHead.innerHTML = json.images[mainThumId].title;
-//
-//         var capCov = document.createElement('p');
-//         capCov.innerHTML = json.images[mainThumId].caption;
-//
-//         var capCovPrice = document.createElement('h1');
-//         capCovPrice.innerHTML = json.images[mainThumId].price + " zł";
-//
-//         var bidSection = document.createElement('div');
-//         bidSection.className = 'col-xs-12';
-//         bidSection.id = 'bidSection';
-//
-//         var leftBid = document.createElement('div');
-//         leftBid.className = 'col-xs-hidden col-md-3';
-//         leftBid.id = 'leftBid';
-//
-//         var mainBid = document.createElement('div');
-//         mainBid.className = 'col-xs-12 col-md-3';
-//         mainBid.id = 'mainBid';
-//
-//         var rightBid = document.createElement('div');
-//         rightBid.className = 'col-xs-hidden col-md-3';
-//         rightBid.id = 'rightBid';
-//
-//         var inputGroup = document.createElement('div');
-//         inputGroup.className = 'input-group';
-//         inputGroup.id = 'bid-input';
-//
-//         var input = document.createElement('input');
-//         input.type = 'number';
-//         input.className = 'form-control';
-//
-//         var spanInput = document.createElement('span');
-//         spanInput.className = 'input-group-addon';
-//         spanInput.innerHTML = "PLN";
-//
-//         var bidBtnWrap = document.createElement('div');
-//         bidBtnWrap.className = 'col-xs-12 col-md-3';
-//         bidBtnWrap.id = 'bid-btn';
-//
-//         var buttonBid = document.createElement('button');
-//         buttonBid.type = 'button';
-//         buttonBid.className = 'btn btn-success';
-//         buttonBid.id = 'bid-button';
-//
-//         var h4BtnBid = document.createElement('h4');
-//         h4BtnBid.innerHTML = "LICYTUJ";
-//
-//         var mainRow = document.createElement('div');
-//         mainRow.className = 'row-fluid';
-//         mainRow.id = 'main-row';
-//
-//         var mainContent = document.getElementById('main-content');
-//         $(mainContent).empty();
-//         mainContent.appendChild(mainRow);
-//         mainRow.appendChild(prevImgCol);
-//         prevImgCol.appendChild(prevImgWrap);
-//         prevImgWrap.appendChild(prevImg);
-//         prevImgWrap.appendChild(arrowPrev);
-//         mainRow.appendChild(mainImgCol);
-//         mainImgCol.appendChild(mainImg);
-//         mainRow.appendChild(nextImgCol);
-//         nextImgCol.appendChild(nextImgWrap);
-//         nextImgWrap.appendChild(nextImg);
-//         nextImgWrap.appendChild(arrowNext);
-//         mainRow.appendChild(prevCapCol);
-//         mainRow.appendChild(mainCapCol);
-//         mainCapCol.appendChild(capCovFlow);
-//         capCovFlow.appendChild(capCovHead);
-//         capCovFlow.appendChild(capCov);
-//         capCovFlow.appendChild(capCovPrice);
-//         mainRow.appendChild(nextCapCol);
-//         mainRow.appendChild(bidSection);
-//         bidSection.appendChild(leftBid);
-//         bidSection.appendChild(mainBid);
-//         mainBid.appendChild(inputGroup);
-//         inputGroup.appendChild(input);
-//         inputGroup.appendChild(spanInput);
-//         bidSection.appendChild(bidBtnWrap);
-//         bidBtnWrap.appendChild(buttonBid);
-//         buttonBid.appendChild(h4BtnBid);
-//         bidSection.appendChild(rightBid);
-//     }
-
-
-//-------------------------------------------------------------------Click on next image in Coverflow event handler
-// $('#main-content').on('click','#nextImgCol .img-responsive', function () {
-//         var imgSrcPrev = json.images[mainImgId].src;
-//         var imgSrc = json.images[mainImgId+1].src;
-//     if(mainImgId+2 < json.images.length){
-//         var imgSrcNext = json.images[mainImgId + 2].src;}
-//         var h3 = json.images[mainImgId+1].title;
-//         var p = json.images[mainImgId+1].caption;
-//         var price = json.images[mainImgId+1].price;
-//
-//         $('#previousImgWrap .img-responsive').removeClass('hidden');
-//         $('#previousImgWrap .arrowPrev').removeClass('hidden');
-//
-//
-//         $('#previousImgWrap').fadeOut(function () {
-//         $('#previousImgWrap .img-responsive').attr('src', imgSrcPrev);});
-//
-//         $('#mainImgCol').fadeOut(function () {
-//             $('#mainImgCol .img-responsive').attr('src', imgSrc);});
-//         $('#mainCapCol').fadeOut(function () {
-//             $('.covFlow h3').text(h3);
-//             $('.covFlow p').text(p);
-//             $('.covFlow h1').text(price + " zł");
-//         });
-//
-//         $('#nextImgWrap').fadeOut(function () {
-//             if(mainImgId+2 < json.images.length){
-//             $('#nextImgCol .img-responsive').attr('src', imgSrcNext);}
-//         });
-//
-//         $('#bidSection').fadeOut();
-//
-//         if(mainImgId == 0){$('#previousImgWrap ').delay(400).fadeIn(500);}
-//         else{$('#previousImgWrap ').fadeIn(500);}
-//         $('#mainImgCol').fadeIn();
-//         $('#mainCapCol').fadeIn();
-//         if(mainImgId+2 < json.images.length){$('#nextImgWrap').fadeIn(500);}
-//          $('#bidSection').fadeIn(500);
-//
-//         mainImgId++;
-//         history.replaceState({ url: '#covFlow' + mainImgId}, null, '#covFlow' + mainImgId);
-// });
-
-//-------------------------------------------------------------------Click on previous image in Coverflow event handler
-// $('#main-content').on('click','#previousImgCol .img-responsive', function () {
-//         if(mainImgId-2 >= 0){
-//             var imgSrcPrev = json.images[mainImgId -2].src;}
-//         var imgSrc = json.images[mainImgId-1].src;
-//         var imgSrcNext = json.images[mainImgId].src;
-//
-//         var h3 = json.images[mainImgId-1].title;
-//         var p = json.images[mainImgId-1].caption;
-//         var price = json.images[mainImgId-1].price;
-//
-//         $('#nextImgWrap .img-responsive').removeClass('hidden');
-//         $('#nextImgWrap .arrowNext').removeClass('hidden');
-//
-//         $('#nextImgWrap').fadeOut(function () {
-//             $('#nextImgWrap .img-responsive').attr('src', imgSrcNext);});
-//
-//         $('#mainImgCol').fadeOut(function () {
-//             $('#mainImgCol .img-responsive').attr('src', imgSrc);});
-//         $('#mainCapCol').fadeOut(function () {
-//             $('.covFlow h3').text(h3);
-//             $('.covFlow p').text(p);
-//             $('.covFlow h1').text(price + " zł");
-//         });
-//
-//         $('#previousImgWrap').fadeOut(function () {
-//             if(mainImgId-2 >= 0){
-//                 $('#previousImgWrap .img-responsive').attr('src', imgSrcPrev);}
-//         });
-//
-//         $('#bidSection').fadeOut();
-//
-//         if(mainImgId == json.images.length-1){$('#nextImgWrap ').delay(400).fadeIn(500);}
-//         else{$('#nextImgWrap').fadeIn(500);}
-//         $('#mainImgCol').fadeIn();
-//         $('#mainCapCol').fadeIn();
-//         if(mainImgId-2 >= 0){$('#previousImgWrap').fadeIn(500);}
-//         $('#bidSection').fadeIn(500);
-//
-//         mainImgId--;
-//         history.replaceState({url: '#covFlow' + mainImgId}, null, '#covFlow' + mainImgId);
-// });
+//-------------------------------------------------------------------Coverflow loading function
 
     function loadCoverFlow(id) {
 
-        mainId = id;
-        var prevId = id - 1;
-        var i = 0;
+        mainId = id;                                                                //initializing variables of main and previous image id
+        var prevId = id - 1;                                                        //and iterator that contains information about id of which
+        var i = 0;                                                                  //image is currently drawn
 
         var mainRow = document.createElement('div');
         mainRow.className = 'row';
@@ -437,7 +197,7 @@ $('#main-content').on('click', '.thumbnail', function () {
 
         //---------------------------------------------------------------------------------FirstElements
 
-        if (mainId > 1) {
+        if (mainId > 1) {                                                           //if main image
 
             for (i; prevId > i; i++) {
                 var listElem = document.createElement('li');
@@ -475,6 +235,7 @@ $('#main-content').on('click', '.thumbnail', function () {
         $(listElem).css('transform', 'translate3d(100%, 0, 0) rotateY(0deg) scale(1)');
         $(listElem).css('opacity', '1');
         $(listElem).css('z-index', '2');
+        listElem.id = 'main';
 
         var img = document.createElement('img');
         img.src = json.images[i].src;
@@ -605,10 +366,15 @@ $('#main-content').on('click', '.thumbnail', function () {
 
     $('#main-content').on('click', '#covFlow #prev', function () {
 
-        var prevImg2 = $('#covList > li:nth-child(' + (mainId - 1) + ')');
-        var prevImg = $('#covList > li:nth-child(' + mainId + ')');
-        var mainImg = $('#covList > li:nth-child(' + (mainId + 1) + ')');
-        var nextImg = $('#covList > li:nth-child(' + (mainId + 2) + ')');
+        var prevImg = $('#covList #prev');
+        var prevImg2 = prevImg.prev();
+        var mainImg = $('#covList #main');
+        var nextImg = $('#covList #next');
+
+        console.log(prevImg2);
+        console.log(prevImg);
+        console.log(mainImg);
+        console.log(nextImg);
 
         prevImg2.css('opacity', '0.6');
         prevImg2.css('z-index', '1');
@@ -617,7 +383,7 @@ $('#main-content').on('click', '.thumbnail', function () {
         prevImg.css('transform', 'translate3d(100%,0,0) rotateY(0) scale(1)');
         prevImg.css('z-index', '2');
         prevImg.css('opacity', '1');
-        prevImg.removeAttr('id');
+        prevImg.attr('id', 'main');
 
         mainImg.css('transform', 'translate3d(200%,0,0) rotateY(45deg) scale(0.8)');
         mainImg.css('z-index', '1');
@@ -651,25 +417,21 @@ $('#main-content').on('click', '.thumbnail', function () {
 //-------------------------------------------------------------------------------------Prev Image Hover
 
     $('#main-content').on('mouseenter', '#covFlow #prev', function () {
-        var prevImg = $('#covList > li:nth-child(' + mainId + ')');
-        prevImg.css('opacity', '1');
+        $(this).css('opacity', '1');
     });
 
     $('#main-content').on('mouseleave', '#covFlow #prev', function () {
-        var prevImg = $('#covList > li:nth-child(' + mainId + ')');
-        prevImg.css('opacity', '0.6');
+       $(this).css('opacity', '0.6');
     });
 
 //-------------------------------------------------------------------------------------Next Image Hover
 
     $('#main-content').on('mouseenter', '#covFlow #next', function () {
-        var prevImg = $('#covList > li:nth-child(' + (mainId+2) + ')');
-        prevImg.css('opacity', '1');
+        $(this).css('opacity', '1');
     });
 
     $('#main-content').on('mouseleave', '#covFlow #next', function () {
-        var prevImg = $('#covList > li:nth-child(' + (mainId+2) + ')');
-        prevImg.css('opacity', '0.6');
+        $(this).css('opacity', '0.6');
     });
 
 
@@ -678,10 +440,10 @@ $('#main-content').on('click', '.thumbnail', function () {
     $('#main-content').on('click', '#covFlow #next', function () {
 
 
-        var prevImg = $('#covList > li:nth-child(' + mainId + ')');
-        var mainImg = $('#covList > li:nth-child(' + (mainId+1) + ')');
-        var nextImg = $('#covList > li:nth-child(' + (mainId+2) + ')');
-        var nextImg2 = $('#covList > li:nth-child(' + (mainId+3) + ')');
+        var prevImg = $('#covList #prev');
+        var mainImg = $('#covList #main');
+        var nextImg = $('#covList #next');
+        var nextImg2 = nextImg.next();
 
         prevImg.css('opacity', '0');
         prevImg.css('z-index', '0');
@@ -695,7 +457,7 @@ $('#main-content').on('click', '.thumbnail', function () {
         nextImg.css('transform', 'translate3d(100%,0,0) rotateY(0) scale(1)');
         nextImg.css('z-index', '2');
         nextImg.css('opacity', '1');
-        nextImg.removeAttr('id');
+        nextImg.attr('id', 'main');
 
         nextImg2.css('opacity', '0.6');
         nextImg2.css('z-index', '1');
@@ -720,7 +482,8 @@ $('#main-content').on('click', '.thumbnail', function () {
 
         history.replaceState({ url: '#covFlow' + mainId}, null, '#covFlow' + mainId);
 
-
     });
+
+
 
 }());
